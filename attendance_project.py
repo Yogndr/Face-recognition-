4f71812c81738540fb533d3598704dd49beb38c9
Yogndr <106022206+Yogndr@users.noreply.github.com>
2022-05-22 23:31:53 +0530

    Add files via upload

---
+++ attendance_project.py
@@ -0,0 +1,89 @@
+import cv2
+import face_recognition
+import numpy as np
+import os
+from datetime import datetime
+
+
+
+path='imagesattendance'
+images=[]
+classNames=[]
+myList=os.listdir(path)
+print(myList)
+for cl in myList:
+    curimg=cv2.imread(f'{path}/{cl}')
+    images.append(curimg)
+    classNames.append(os.path.splitext(cl)[0])
+    print(classNames)
+
+def findencodings(images):
+    enocdelist=[]
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode =face_recognition.face_encodings(img)[0]
+        enocdelist.append(encode)
+    return enocdelist
+
+
+def markAttendance(name):
+    with open('Attendance.csv','r+') as f:
+        mydatalist =f.readlines()
+        namelist=[]
+        for line in mydatalist:
+            entry=line.split(',')
+            namelist.append(entry[0])
+        if name not in namelist:
+            time_now = datetime.now()
+            tStr=time_now.strftime('%H:%M:%S')
+            dStr =time_now.strftime('%d:%m:%y')
+            f.writelines(f'\n{name},{dStr},{tStr}')
+
+
+
+encodelistknown = findencodings(images)
+print('encoding complete')
+
+cap=cv2.VideoCapture(0)
+while True:
+    success,img=cap.read()
+    imgS=cv2.resize(img,(0,0),None,0.25,0.25)
+    imgS=cv2.cvtColor(imgS,cv2.COLOR_BGR2RGB)
+
+    facesCurrFrame = face_recognition.face_locations(imgS)
+    encodesCurrFrame= face_recognition.face_encodings(imgS,facesCurrFrame)
+
+    for encodeface,faceloc in zip(encodesCurrFrame,facesCurrFrame):
+        matches=face_recognition.compare_faces(encodelistknown,encodeface)
+        faceDis= face_recognition.face_distance(encodelistknown,encodeface)
+        # print(faceDis)
+        matchIndex=np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            name=classNames[matchIndex].upper()
+            # print(name)
+            y1,x2,y2,x1=faceloc
+            y1, x2, y2, x1=y1*4,x2*4,y2*4,x1*4
+            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+            markAttendance(name)
+
+    cv2.imshow('Webcam',img)
+    if cv2.waitKey(10)==13:
+        break
+        cap.release()
+        cv2.destroyAllWindows()
+
+
+
+# faceloc= face_recognition.face_locations(imgsrk)[0]
+# encodesrk= face_recognition.face_encodings(imgsrk)[0]
+# cv2.rectangle(imgsrk,(faceloc[3],faceloc[0]),(faceloc[1],faceloc[2]),(255,0,255),2)
+#
+# faceloctest= face_recognition.face_locations(imgtest)[0]
+# encodetest= face_recognition.face_encodings(imgtest)[0]
+# cv2.rectangle(imgtest,(faceloctest[3],faceloctest[0]),(faceloctest[1],faceloctest[2]),(255,0,255),2)
+#
+# results= face_recognition.compare_faces([encodesrk],encodetest)
+# faceDis = face_recognition.face_distance([encodesrk],encodetest)
